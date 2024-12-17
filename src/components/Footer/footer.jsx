85c1c07f48fcc20a534cce280da479f7b43c1e8a
Leo-Brd <leobordet.pro@gmail.com>
2024-12-17 17:58:19 +0100

    Add footer structure

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,11 +1,17 @@
 import './footer.scss'
+import logo from '../../assets/logoFooter.png'
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-    <div>
-        <h1>Footer</h1>
-    </div>
-);
+        <footer>
+            <Link to="/">
+                <img src={logo} alt="Logo de Kasa"/>
+            </Link>
+
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,26 +1,26 @@
 import './header.scss'
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logoHeader.svg'
 import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-    <header>
-        <Link to="/">
-            <img src={logo} alt="Logo de Kasa"/>
-        </Link>
+        <header>
+            <Link to="/">
+                <img src={logo} alt="Logo de Kasa"/>
+            </Link>
 
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Accueil</Link>
-                </li>
-                <li>
-                    <Link to="/About">A Propos</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
-);
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Accueil</Link>
+                    </li>
+                    <li>
+                        <Link to="/About">A Propos</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
