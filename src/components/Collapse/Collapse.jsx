f147e86380170be548d21f9ff8030d8ca010f2d5
Leo-Brd <leobordet.pro@gmail.com>
2024-12-20 18:29:26 +0100

    Add collapses to about page

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,17 +1,20 @@
-import './Collapse.scss'
+import './Collapse.scss';
 import { useState } from 'react';
+import chevron_up from '../../assets/images/chevron_up.svg'
+import chevron_down from '../../assets/images/chevron_down.svg'
 
-function Collapse ( {title, children} ) {
+function Collapse({ title, children }) {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className='collapse'>
-            <div className='collapse__header'>
+        <div className="collapse">
+            <div className="collapse__header" onClick={() => setIsOpen(!isOpen)}>
                 <h2>{title}</h2>
-                <FontAwesomeIcon icon="fa-solid fa-chevron-up" />
+                <img src={isOpen ? chevron_up : chevron_down} alt="Chevron" />
             </div>
+            {isOpen && <div className="collapse__content">{children}</div>}
         </div>
-    )
+    );
 }
 
 export default Collapse;
